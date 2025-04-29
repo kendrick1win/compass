b18757d742a727c9686b8188515cc5c427edbc8b
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2025-04-29 12:47:03 +0100

    protect profile page

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,7 +1,16 @@
+import { redirect } from "next/navigation";
 import ProfileForm from "./(components)/Profile";
 import Header from "@/components/custom/header";
+import { createClient } from "@/utils/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
   return (
     <main>
       <Header />
